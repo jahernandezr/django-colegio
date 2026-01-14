3f7743bf81ddd1183aa93fd59b162e6490c66ec0
Jhon Alexander <alexpill01@gmail.com>
2026-01-13 22:18:16 -0500

    version 2

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import cursos, materias
 
 # Register your models here.
+
+admin.site.register(cursos)
+admin.site.register(materias)
--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class cursos(models.Model):
+    nombre = models.CharField(max_length=50)
+    cantidad_alumnos = models.IntegerField()
+    tipo_curso = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class materias(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=20)
+    id_colegio = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre    
