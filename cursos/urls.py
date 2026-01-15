999819672b4f3f490252771af24af78a9f6059ba
Jhon Alexander <alexpill01@gmail.com>
2026-01-14 21:29:03 -0500

    version 3

--- cursos/urls.py
+++ cursos/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import  views
 
 urlpatterns =[
-    path('listar', views.litasdecursos , name='listas-cursos')
+    path('listar', views.litasdecursos , name='listas-cursos'),
+    path('editar/<int:id>',views.editarcursos , name="editar-curso"),
+    path('eliminar/<int:id>',views.deletecurso ,  name="delete-curso"),
+    path('agregar', views.agregarcursos , name="agregar-curso"),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
+from . models  import cursos
 
 def litasdecursos(request):
-    return render(request , 'cursos/listarcursos.html')
+    curso_lista= cursos.objects.all()
+    data= {
+        'cursos': curso_lista
+    }
+    return render(request , 'cursos/listarcursos.html',data)
+
+def editarcursos(request,id):
+    curso = get_object_or_404(cursos, id=id)
+    data ={
+        'curso' :curso
+    }
+        
+    if request.method == 'POST':
+        curso.nombre = request.POST['nombre']
+        curso.cantidad_alumnos = request.POST['cantidad_alumnos']
+        curso.tipo_curso = request.POST['tipo_curso']
+        curso.save()
+        return  redirect('listas-cursos')
+    
+    return render(request, 'cursos/editar.html',data)
+
+def deletecurso(request, id):
+    curso =  get_object_or_404(cursos, id=id)
+    curso.delete()
+
+    return redirect('listas-cursos')
+
+def agregarcursos(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        cantidad = request.POST.get('cantidad_alumnos')
+        tipo = request.POST.get('tipo_curso')
+
+        if not nombre or not cantidad or not tipo:
+            return render(request, 'cursos/agregar.html',{'error :' : 'Digitar todos los campos'})
+        
+        cursos.objects.create(
+            nombre = nombre,
+            cantidad_alumnos = int (cantidad) ,
+            tipo_curso = tipo
+        )
+
+        return redirect('listas-cursos')
+    return render(request , 'cursos/agregar.html' )
+
+        
